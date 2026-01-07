d478afecb192c40264a6342e03eba0659dd0d1cc
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-01-07 11:20:40 +0530

    Add or update file: test_add.py

---
+++ tests/test_add.py
@@ -0,0 +1,11 @@
+import pytest
+from src.math_operations import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
+
+def test_add_mixed_numbers():
+    assert add(-2, 3) == 1
