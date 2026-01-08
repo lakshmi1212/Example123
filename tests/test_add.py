ea44fb35a9180eac3748fb520c2576669eee189c
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-01-08 15:51:26 +0530

    Add or update file: test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -10,6 +10,5 @@ def test_add_negative_numbers():
 def test_add_mixed_numbers():
     assert add(-3, 5) == 2
 
-def test_add_zero():
+def test_add_with_zero():
     assert add(0, 5) == 5
-    assert add(5, 0) == 5
